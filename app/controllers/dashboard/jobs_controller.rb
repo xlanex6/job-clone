3a6679508d9e4fcd57266724875b97beaf681b62
Alex <alexduval71@gmail.com>
2016-08-16 19:32:56 +0200

    simple_form_for Jobs #issue on user_id

--- app/controllers/dashboard/jobs_controller.rb
+++ app/controllers/dashboard/jobs_controller.rb
@@ -1,14 +1,34 @@
-class JobsController
+class Dashboard::JobsController < ApplicationController
 
   def index
-
+    @jobs = current_user.jobs.all
   end
 
   def show
 
   end
 
+  def new
+    @job = Job.new
+  end
+
   def create
 
+    @job = current_user.Job.new(job_params)
+    if @job.save
+      render :index
+    else
+      render 'new'
+    end
   end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :content, :address, :start_time, :end_time, :user_id)
+  end
+
+
+
 end
