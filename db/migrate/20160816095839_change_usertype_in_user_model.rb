2c31e19a271dc3582e30d64fc98fb1b17ee6a15e
Alex <alexduval71@gmail.com>
2016-08-16 12:02:13 +0200

    change user_types on user model

---
+++ db/migrate/20160816095839_change_usertype_in_user_model.rb
@@ -0,0 +1,6 @@
+class ChangeUsertypeInUserModel < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :user_type
+    add_column :users, :user_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816084907) do
+ActiveRecord::Schema.define(version: 20160816095839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 20160816084907) do
     t.string   "cv"
     t.string   "website"
     t.string   "company"
-    t.boolean  "user_type"
     t.integer  "skill_id"
+    t.string   "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["skill_id"], name: "index_users_on_skill_id", using: :btree
