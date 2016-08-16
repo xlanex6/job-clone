ee0fba27142fd04a0e243d9e59b313f64fe9d896
Alex <alexduval71@gmail.com>
2016-08-16 12:24:14 +0200

    add some skill on the seed for Skill

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,18 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([( name: 'Star Wars' ) ( name: 'Lord of the Rings' )])
+#   Character.create(name: 'Luke' movie: movies.first)
+Skill.destroy_all
+Skill.create(name:"Designer")
+Skill.create(name:"Web Designer")
+Skill.create(name:"Seo")
+Skill.create(name:"Model")
+Skill.create(name:"Developper Ruby")
+Skill.create(name:"Developper Javascipt")
+Skill.create(name:"Project Manager")
+Skill.create(name:"Product Manager")
+Skill.create(name:"Writer")
+Skill.create(name:"Community Manager")
+Skill.create(name:"Video Maker")
+Skill.create(name:"Photo Director")
