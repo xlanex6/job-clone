b58302fc92be52beef52c1e020427be5c778aed2
Vincent Hamonic <v.hamonic@gmail.com>
2016-08-17 10:22:38 +0200

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([( name:  Star Wars  ) ( name:  Lord of the Rings  )])
 #   Character.create(name:  Luke  movie: movies.first)
-Skill.destroy_all
 Skill.create(name:"Designer")
 Skill.create(name:"Web Designer")
 Skill.create(name:"Seo")
