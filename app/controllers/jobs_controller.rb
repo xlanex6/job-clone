7cb3d132b982235779edf293bd6c6577793f3311
Cyril Fougeres <cyril.fougeres@gmail.com>
2016-08-18 18:28:22 +0200

    geo loc job -> OK

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,48 @@
 class JobsController < ApplicationController
 
   def index
+
     if params[:jobs].nil?
       @jobs = Job.all
-    else
-      @jobs = Job.where(skill_id: params[:jobs][:skill_id]).near(params[:jobs][:address], '10')
+      @geo_job = Job.near('world', '10')
 
       @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
         marker.lat job.latitude
         marker.lng job.longitude
         marker.infowindow render_to_string(partial: "/jobs/map_box", locals: { job: job })
+      end
+    else
+      if params[:jobs][:address].empty? && params[:jobs][:skill_id].empty?
+        @jobs = Job.all
+        @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+          marker.lat job.latitude
+          marker.lng job.longitude
+          marker.infowindow render_to_string(partial: "/jobs/map_box", locals: { job: job })
+          end
+      else
+        if params[:jobs][:skill_id].empty?
+          @jobs = Job.all.near(params[:jobs][:address], '10')
+          @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+            marker.lat job.latitude
+            marker.lng job.longitude
+            marker.infowindow render_to_string(partial: "/jobs/map_box", locals: { job: job })
+            end
+        elsif params[:jobs][:address].empty?
+          @jobs = Job.where(skill_id: params[:jobs][:skill_id])
+          @geo_job = Job.near('world', '10')
+          @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+            marker.lat job.latitude
+            marker.lng job.longitude
+            marker.infowindow render_to_string(partial: "/jobs/map_box", locals: { job: job })
+            end
+        else
+          @jobs = Job.where(skill_id: params[:jobs][:skill_id]).near(params[:jobs][:address], '10')
+          @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+            marker.lat job.latitude
+            marker.lng job.longitude
+            marker.infowindow render_to_string(partial: "/jobs/map_box", locals: { job: job })
+            end
+        end
       end
     end
   end
